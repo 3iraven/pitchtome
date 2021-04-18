5ebe89efd738d810be02e89d77820e180f18c571
3iraven <34956807+3iraven@users.noreply.github.com>
2021-04-18 13:55:09 -0700

    services

--- src/App.js
+++ src/App.js
@@ -59,36 +59,42 @@ function App() {
       <div className="row">
         <div className="col-lg-12 text-center">
           <h2 className="section-heading text-uppercase">Services</h2>
-          <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+          <h3 className="section-subheading text-muted">Pitch Coach is a non-profit organization designed to provide these services:</h3>
         </div>
       </div>
       <div className="row text-center">
-        <div className="col-md-4">
-          <span className="fa-stack fa-4x">
-            <i className="fa fa-circle fa-stack-2x text-primary"></i>
-            <i className="fa fa-shopping-cart fa-stack-1x fa-inverse"></i>
-          </span>
-          <h4 className="service-heading">E-Commerce</h4>
-          <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
-        </div>
-        <div className="col-md-4">
-          <span className="fa-stack fa-4x">
-            <i className="fa fa-circle fa-stack-2x text-primary"></i>
-            <i className="fa fa-laptop fa-stack-1x fa-inverse"></i>
-          </span>
-          <h4 className="service-heading">Responsive Design</h4>
-          <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
-        </div>
-        <div className="col-md-4">
-          <span className="fa-stack fa-4x">
-            <i className="fa fa-circle fa-stack-2x text-primary"></i>
-            <i className="fa fa-lock fa-stack-1x fa-inverse"></i>
-          </span>
-          <h4 className="service-heading">Web Security</h4>
-          <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
-        </div>
-      </div>
+        
+
+  <div className="col-md-4">
+    <div class="card">
+      <h3>Coaching</h3>
+      <p>Build your speach</p>
+      <p>Some text</p>
     </div>
+  </div>
+
+  <div className="col-md-4">
+    <div class="card">
+      <h3>Matching</h3>
+      <p>Connecting...</p>
+      <p>Some text</p>
+    </div>
+  </div>
+  
+  <div className="col-md-4">
+    <div class="card">
+      <h3>Growth</h3>
+      <p>Education/Training...</p>
+      <p>Some text</p>
+    </div>
+  </div>
+  
+        
+      </div>
+      
+    </div>
+    <h3 className="section-subheading text-muted">Pitch Coach is AI ...</h3>
+
   </section>
 
   
