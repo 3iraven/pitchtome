9d198acbbea5f3aa0553b269f9fd114d17117412
3iraven <34956807+3iraven@users.noreply.github.com>
2021-04-18 00:14:29 -0700

    UPdate

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App() {
     <div className="App">
       <nav className="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
     <div className="container">
-      <a className="navbar-brand js-scroll-trigger" href="#page-top">Start Bootstrap</a>
+      <a className="navbar-brand"  href="#page-top" >Pitch Coach</a>
       <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         Menu
         <i className="fa fa-bars"></i>
@@ -69,11 +69,21 @@ function App() {
   
   <header className="masthead">
     <div className="container">
+
+    <div class="row"> 
+      <div class="col">
+
       <div className="intro-text">
-        <div className="intro-lead-in">Welcome To Our Studio!</div>
-        <div className="intro-heading text-uppercase">It's Nice To Meet You</div>
-        <a className="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Tell Me More</a>
+        <div className="intro-lead-in">Welcome To Pitch Coach</div>
+        <div className="intro-heading text-uppercase">Thanks for joining us</div>
+        <a className="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Join the movement</a>
       </div>
+      </div>
+      <div class="col right-column"></div> 
+   </div> 
+
+
+  
     </div>
   </header>
 
