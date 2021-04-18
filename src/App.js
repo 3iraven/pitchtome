97d4de17e4980e101c0afc35c6828ba1d635b407
3iraven <34956807+3iraven@users.noreply.github.com>
2021-04-18 13:27:12 -0700

    team

--- src/App.js
+++ src/App.js
@@ -127,15 +127,15 @@ function App() {
       <div className="row">
         <div className="col-lg-12 text-center">
           <h2 className="section-heading text-uppercase">Our Amazing Team</h2>
-          <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+          <h3 className="section-subheading text-muted">Meet our Founders.</h3>
         </div>
       </div>
       <div className="row">
         <div className="col-sm-4">
           <div className="team-member">
             <img className="mx-auto rounded-circle" src="img/team/1.jpg" alt=""/>
-            <h4>Kay Garland</h4>
-            <p className="text-muted">Lead Designer</p>
+            <h4>Nike</h4>
+            <p className="text-muted">Co-founder</p>
             <ul className="list-inline social-buttons">
               <li className="list-inline-item">
                 <a href="#something">
@@ -158,8 +158,8 @@ function App() {
         <div className="col-sm-4">
           <div className="team-member">
             <img className="mx-auto rounded-circle" src="img/team/2.jpg" alt=""/>
-            <h4>Larry Parker</h4>
-            <p className="text-muted">Lead Marketer</p>
+            <h4>Stephanie</h4>
+            <p className="text-muted">Co-founder</p>
             <ul className="list-inline social-buttons">
               <li className="list-inline-item">
                 <a href="#something">
@@ -182,8 +182,8 @@ function App() {
         <div className="col-sm-4">
           <div className="team-member">
             <img className="mx-auto rounded-circle" src="img/team/3.jpg" alt=""/>
-            <h4>Diana Pertersen</h4>
-            <p className="text-muted">Lead Developer</p>
+            <h4>Jeremie</h4>
+            <p className="text-muted">Co-founder</p>
             <ul className="list-inline social-buttons">
               <li className="list-inline-item">
                 <a href="#something">
@@ -246,7 +246,6 @@ function App() {
       <div className="row">
         <div className="col-lg-12 text-center">
           <h2 className="section-heading text-uppercase">Contact Us</h2>
-          <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
         </div>
       </div>
       <div className="row">
