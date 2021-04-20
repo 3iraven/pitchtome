26e79dc3354fa340ead8eea30a4765330a111512
3iraven <34956807+3iraven@users.noreply.github.com>
2021-04-20 09:59:02 -0700

    Initial for Blue Theme

--- src/index.js
+++ src/index.js
@@ -2,13 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-import './assets/css/bootstrap.min.css'
-import './assets/css/agency.min.css'
+import Header from "./components/Header"
+import { CssBaseline } from "@material-ui/core"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import reportWebVitals from './reportWebVitals';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const rootElement = document.getElementById("root")
+ReactDOM.render(
+  <React.StrictMode>
+    <CssBaseline />
+    <Header /> {/*Add this*/}
+    <App />
+  </React.StrictMode>,
+  rootElement
+)
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
